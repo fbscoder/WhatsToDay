01d4f5fa391fee59c32d8e89ea68975749c56ef2
Christof Pliessnig <c.pliessnig@xgx.at>
2018-11-26 11:13:41 +0100

    [UPDATE] Added Symfony Project

---
+++ src/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/", name="blog_list")
+     */
+    public function index()
+    {
+        return new Response('Test Controller');
+    }
+
+}
---
+++ src/Controller/main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gajac
+ * Date: 26.11.2018
+ * Time: 08:43
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class main extends AbstractController
+{
+
+}
