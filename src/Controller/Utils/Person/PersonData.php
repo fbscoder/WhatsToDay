60b2132ecd1d05fbaa094cbe24f842df69634d45
Marcell <marcell.koenig@it-lab.ktn.at>
2018-11-29 14:02:43 +0100

    Test: API_KEY
    Signed-off-by: Marcell <marcell.koenig@it-lab.ktn.at>

--- src/Controller/Utils/Person/PersonData.php
+++ src/Controller/Utils/Person/PersonData.php
@@ -4,24 +4,17 @@ namespace App\Controller\Utils\Person;
 
 class PersonData{
     public $ID;
-    public $API_KEY;
     public $TOKEN;
 
-    public function __construct($ID = null, $API_KEY = null, $TOKEN = null)
+    public function __construct($ID = null, $TOKEN = null)
     {
         $this->ID = $ID;
-        $this->API_KEY = $API_KEY;
         $this->TOKEN = $TOKEN;
     }
 
     public function setID($ID)
     {
         $this->ID = $ID;
-    }
-
-    public function setAPI_KEY($API_KEY)
-    {
-        $this->API_KEY = $API_KEY;
     }
 
     public function setTOKEN($TOKEN)
