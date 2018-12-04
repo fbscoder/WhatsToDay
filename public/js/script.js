447b730eb970777e16e3d970ff11be3c86327224
Marcell <marcell.koenig@it-lab.ktn.at>
2018-12-04 12:47:19 +0100

    Boards
    Signed-off-by: Marcell <marcell.koenig@it-lab.ktn.at>

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
     $("#hideTasks").click(function () {
         $("#tasks").css({'overflow': "hidden"});
+        $("#tasks").css({'white-space': "nowrap"});
         $("#tasks").removeClass("show");
     });
     $("#btn_today").click(function () {
         $("#task_tomorrow").css({'display': "none"});
         $("#task_today").css({'display': "block"});
         $("#tasks").addClass("show");
+        $("#tasks").css({'white-space': "nowrap"});
 
-        $("#tasks").css({'overflow': "visible"});
+        $("#tasks").one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
+            function () {
+                $("#tasks").css({'white-space': "normal"});
+                $("#tasks").css({'overflow': "visible"});
+            });
     });
     $("#btn_tomorrow").click(function () {
         $("#task_today").css({'display': "none"});
         $("#task_tomorrow").css({'display': "block"});
         $("#tasks").addClass("show");
+        $("#tasks").css({'white-space': "nowrap"});
 
-        $("#tasks").css({'overflow': "visible"});
+        $("#tasks").one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
+            function () {
+                $("#tasks").css({'white-space': "normal"});
+                $("#tasks").css({'overflow': "visible"});
+            });
     });
 });
