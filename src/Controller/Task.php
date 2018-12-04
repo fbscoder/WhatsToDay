31b448b20ba21305ab7243b0a8ae64c046bcffc9
Christof Pliessnig <c.pliessnig@xgx.at>
2018-12-04 08:17:31 +0100

    UPDATE Added possibility to select more boards
    Signed-off-by: Christof Pliessnig <c.pliessnig@xgx.at>

--- src/Controller/Task.php
+++ src/Controller/Task.php
@@ -20,13 +20,40 @@ class Task extends AbstractController
      */
     public function showTask()
     {
-        $params['taskToday'] = APIUtils::getBoardTasksToday($_GET['board']);
+        $boards = json_decode($_GET['selectedBoards']);
+        $count = 0;
+        foreach ($boards as $i => $board) {
+            $params['taskToday'] = APIUtils::getBoardTasksToday($board);
+            $params['taskTomorrow'] = APIUtils::getBoardTasksTomorrow($board);
+
+            foreach ($params['taskToday'] as $c => $task) {
+                $tasks[$count]['title'] = $task->title;
+                $tasks[$count]['description'] = $task->description;
+                $tasks[$count]['checkListData'] = $task->checkListData;
+                $tasks[$count]['id'] = $task->id;
+                $count++;
+            }
+            foreach ($params['taskTomorrow'] as $c => $task) {
+                $tasksTomorrow[$count]['title'] = $task->title;
+                $tasksTomorrow[$count]['description'] = $task->description;
+                $tasksTomorrow[$count]['checkListData'] = $task->checkListData;
+                $tasksTomorrow[$count]['id'] = $task->id;
+                $count++;
+            }
+        }
+
+        $params['taskToday'] = $tasks;
+        $params['taskTomorrow'] = $tasksTomorrow;
+
+        $params['taskToday'] = array_values($params['taskToday']);
+        $params['taskTomorrow'] = array_values($params['taskTomorrow']);
+
         $params['taskTodayCount'] = APIUtils::getCountedTasks($params['taskToday']);
-        $params['taskTomorrow'] = APIUtils::getBoardTasksTomorrow($_GET['board']);
-        $params['taskTomorrowCount'] = APIUtils::getCountedTasks(APIUtils::getBoardTasksTomorrow($_GET['board']));
+        $params['taskTomorrowCount'] = APIUtils::getCountedTasks($params['taskTomorrow']);
+
         if (isset($_POST['card_id']))
             APIUtils::setTaskToFinish($_POST["card_id"]);
-
+        #$test = $this->render('yourTemplate.html.twig')->getContent();
         return $this->render('aufgaben.html.twig', $params);
     }
 }
